c6e6edab05d589e3ebbafcad3d005a0075527168
Roman Tsukanov <roman@tsukanov.me>
2016-10-05 04:35:01 +0200

    Update perl config for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
-eval (perl -Mlocal::lib)
+set -x PERL_MM_OPT "INSTALL_BASE=$HOME/perl5"
+eval (perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
+
 set PATH ~/Library/Android/sdk/platform-tools $PATH
 set PATH /usr/local/sbin $PATH
 set -x GOPATH ~/workspace/go
