6f73b534ae593bf3dce2933195b972599dfd6114
Roman Tsukanov <roman@tsukanov.me>
2016-11-15 15:17:11 +0100

    Cleanup fish config and add a function for cleaning Docker

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,25 @@
+set PATH /usr/local/sbin $PATH
+
+# Perl
 set -x PERL_MM_OPT "INSTALL_BASE=$HOME/perl5"
 eval (perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)
 
+# Android tools
 set PATH ~/Library/Android/sdk/platform-tools $PATH
-set PATH /usr/local/sbin $PATH
+
+# Add directory with "subl" command to lunch Sublime Text
+set PATH /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/ $PATH
+
+# Golang
 set -x GOPATH ~/workspace/go
 set PATH $GOPATH/bin $PATH
 
+# fuck
 eval (thefuck --alias | tr '\n' ';')
+
+####################
+# Helper functions #
+####################
 
 function docker-bash
 	docker exec -it $argv /bin/bash
@@ -14,4 +27,12 @@ end
 
 function docker-sh
     docker exec -it $argv /bin/sh
+end
+
+function docker-clean
+	echo "Removing containers..."
+	docker rm -f (docker ps -a -q)
+	echo "Removing images..."
+	docker rmi -f (docker images -q)
+	docker ps -a | cut -c-12 | xargs docker rm
 end
