7050a97f1773198e5816425f127b4e814a82c199
Roman Tsukanov <git@roman.zone>
2025-09-30 16:09:36 -0700

    Fish updates

--- fish/config.fish
+++ fish/config.fish
@@ -12,12 +12,6 @@ if type -q /opt/homebrew/bin/brew
     eval "$(/opt/homebrew/bin/brew shellenv)"
 end
 
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 if status is-interactive
     if type -q atuin
         atuin init fish | source
